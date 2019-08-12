29227ad578cc084278c0dd1b264f259d0af40b3c
Nolan Prochnau <parvus.mortalis@gmail.com>
2019-08-12 17:02:01 -0400

    Add documentation

--- lib/warpera.rb
+++ lib/warpera.rb
@@ -1,8 +1,14 @@
+# Deals with years possibly spanning different eras
 module Warpera
   require 'warpera/pair'
 
+  ##
+  # Convert an integer-formatted year into a string-formatted year
+  #
+  # @param year [Integer] the integer-formatted year
+  # @return [String] the string-formatted year
   def self.conv_s(year)
-    raise ArgumentError, 'Argument is not numeric' unless year.is_a? Numeric
+    raise ArgumentError, 'Argument is not an integer' unless year.is_a? Integer
 
     if year < 0
       "#{-1 * year} BCE"
@@ -11,6 +17,11 @@ module Warpera
     end
   end
 
+  ##
+  # Convert a string-formatted year into an integer-formatted year
+  #
+  # @param year [String] the string-formatted year
+  # @return [Integer] the integer-formatted year
   def self.conv_i(year)
     raise ArgumentError, 'Argument is not a string' unless year.is_a? String
 
--- lib/warpera/pair.rb
+++ lib/warpera/pair.rb
@@ -1,7 +1,20 @@
 module Warpera
+  # Pairs year and era together for better readability and DRYing
   class Pair
-    attr_accessor :year, :era
 
+    # The year as a whole number
+    # @return [Integer] the year
+    attr_accessor :year
+
+    # The era
+    # @return [:ce, :bce]
+    attr_accessor :era
+
+    ##
+    # Sets the attributes
+    #
+    # @param year [Integer] the year
+    # @param era [:ce, :bce] the era
     def initialize(year:, era:)
       @year = year
       @era = era
@@ -10,10 +23,32 @@ module Warpera
       raise ArgumentError, 'Era is not valid' unless valid_era?
     end
 
+    ##
+    # Format the year and era into a string
+    #
+    # @example Current Era
+    #   >> Warpera::Pair.new(year: 2019, era: :ce).to_s
+    #   => "2019 CE"
+    # @example Previous Era
+    #   >> Warpera::Pair.new(year: 2019, era: :bce).to_s
+    #   => "2019 BCE"
+    #
+    # @return [String] the formatted year
     def to_s
       "#{@year} #{@era.upcase}"
     end
 
+    ##
+    # Format the year and era into an integer
+    #
+    # @example Current Era
+    #   >> Warpera::Pair.new(year: 2019, era: :ce).to_s
+    #   => 2019
+    # @example Previous Era
+    #   >> Warpera::Pair.new(year: 2019, era: :bce).to_s
+    #   => -2019
+    #
+    # @return [Integer] the formatted year
     def to_i
       if @era == :bce
         return -1 * @year
